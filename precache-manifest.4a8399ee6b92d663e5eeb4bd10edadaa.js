d6f5bef0818fd6770a1a93a5bae4d0d4967c433e
iheathers <shresthapritam7@gmail.com>
2020-07-22 13:10:03 +0545

    Updates

---
+++ precache-manifest.4a8399ee6b92d663e5eeb4bd10edadaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87423178d83ba2ba8aeb2bda43372ed1",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "6ca4dbaa8e1b188d9527",
+    "url": "/seasons/static/css/main.489174fb.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca4dbaa8e1b188d9527",
+    "url": "/seasons/static/js/main.6161f2ed.chunk.js"
+  },
+  {
+    "revision": "7aa38b62a80534558f86",
+    "url": "/seasons/static/js/runtime-main.53d2d0be.js"
+  }
+]);
